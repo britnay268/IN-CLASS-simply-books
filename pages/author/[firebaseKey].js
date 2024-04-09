8a601a505b6398ad600c682b052885f795e3f5de
Britnay Gore <britnaygore268@gmail.com>
2024-04-08 21:31:13 -0400

    You can see the books that the authors have when view is clicked on author

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import { viewAuthorDetails } from '../../api/mergedData';
 
 export default function ViewAuthor() {
@@ -23,6 +23,29 @@ export default function ViewAuthor() {
           <Button variant="info">Edit</Button>
         </Link>
         <Button variant="danger" className="m-2">Delete</Button>
+      </div>
+      <div>
+        {authorDetails.books?.map((bookObj) => (
+
+          <Card style={{ width: '18rem', margin: '10px' }}>
+            <Card.Img variant="top" src={bookObj.image} alt={bookObj.title} style={{ height: '400px' }} />
+            <Card.Body>
+              <Card.Title>{bookObj.title}</Card.Title>
+              <p className="card-text bold">{bookObj.sale && <span>SALE<br /></span> } ${bookObj.price}</p>
+              {/* DYNAMIC LINK TO VIEW THE BOOK DETAILS  */}
+              <Link href={`/book/${bookObj.firebaseKey}`} passHref>
+                <Button variant="primary" className="m-2">VIEW</Button>
+              </Link>
+              {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
+              <Link href={`/book/edit/${bookObj.firebaseKey}`} passHref>
+                <Button variant="info">EDIT</Button>
+              </Link>
+              <Button variant="danger" className="m-2">
+                DELETE
+              </Button>
+            </Card.Body>
+          </Card>
+        ))}
       </div>
     </>
   );
