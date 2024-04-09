91c3085f0f8d01c87b255bba570e04303fe0f460
Britnay Gore <britnaygore268@gmail.com>
2024-04-08 21:53:45 -0400

    When you delete the author, the book delets as well

--- components/AuthorCard.js
+++ components/AuthorCard.js
@@ -2,8 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import Link from 'next/link';
+import { deleteAuthorBooks } from '../api/mergedData';
 
-export default function AuthorCard({ authorObj }) {
+export default function AuthorCard({ authorObj, onUpdate }) {
+  const deleteTheAuthor = () => {
+    if (window.confirm(`Delete ${authorObj.first_name} ${authorObj.last_name}?`)) {
+      deleteAuthorBooks(authorObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
   return (
     <Card style={{ width: '18rem', margin: '10px' }}>
       <Card.Body>
@@ -16,7 +23,7 @@ export default function AuthorCard({ authorObj }) {
         <Link href={`/author/edit/${authorObj.firebaseKey}`} passHref>
           <Button variant="info">Edit</Button>
         </Link>
-        <Button variant="danger" className="m-2">Delete</Button>
+        <Button variant="danger" className="m-2" onClick={deleteTheAuthor}>Delete</Button>
       </Card.Body>
     </Card>
   );
@@ -29,4 +36,5 @@ AuthorCard.propTypes = {
     last_name: PropTypes.string,
     firebaseKey: PropTypes.string,
   }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
