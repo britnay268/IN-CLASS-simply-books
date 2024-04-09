eaa2552a7f12e597ed5b8460f8f332e15c5a3cfd
Britnay Gore <britnaygore268@gmail.com>
2024-04-08 21:17:25 -0400

    Show author name, email and edit and delete button when view button is clicked

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -1,13 +1,29 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { viewAuthorDetails } from '../../api/mergedData';
 
 export default function ViewAuthor() {
+  const [authorDetails, setAuthorDetails] = useState({});
+
   const router = useRouter();
   const { firebaseKey } = router.query;
-  console.warn(firebaseKey);
+
+  useEffect(() => {
+    viewAuthorDetails(firebaseKey).then(setAuthorDetails);
+  }, [firebaseKey]);
 
   return (
     <>
-      View Author
+      <div className="text-white ms-5 details">
+        <h5>{authorDetails.first_name} {authorDetails.last_name}</h5>
+        <h6>Author Email: {authorDetails.email}</h6>
+        <Link href={`/author/edit/${authorDetails.firebaseKey}`} passHref>
+          <Button variant="info">Edit</Button>
+        </Link>
+        <Button variant="danger" className="m-2">Delete</Button>
+      </div>
     </>
   );
 }
