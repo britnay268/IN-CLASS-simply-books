6db876aab525b8c298f95074133e6c734a995e63
Britnay Gore <britnaygore268@gmail.com>
2024-04-09 21:41:37 -0400

    Added structure for form

--- components/forms/AuthorForm.js
+++ components/forms/AuthorForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
 
 const initialState = {
   first_name: '',
@@ -9,10 +10,38 @@ const initialState = {
 
 export default function AuthorForm({ obj }) {
   return (
-    <div>
-      Author Form here
-      {console.warn(obj)}
-    </div>
+    <Form>
+      <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Author</h2>
+
+      <FloatingLabel controlId="floatingInput1" label="Author First Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter a first name"
+          name="first_name"
+          required
+        />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingInput2" label="Author Last Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter a last name"
+          name="last_name"
+          required
+        />
+      </FloatingLabel>
+
+      <FloatingLabel controlId="floatingInput1" label="Author Email" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter an email"
+          name="email"
+          required
+        />
+      </FloatingLabel>
+
+      <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Author</Button>
+    </Form>
   );
 }
 
